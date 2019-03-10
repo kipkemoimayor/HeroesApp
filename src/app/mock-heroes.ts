7c6127e745077db32b6e25a859c1a6ac3b8b6cc4
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-03-10 21:44:23 +0300

    Initial comiit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from '../hero';
+export const HEROES:Hero[]=[
+  {id:11,name:"Dr.Strange"},
+  {id:12,name:"Mr.Nice"},
+  {id:13,name:"Batman"},
+  {id:14,name:"Wolverine"},
+  {id:15,name:"Black Panther"},
+  {id:16,name:"Thor"},
+  {id:17,name:"Captain America"},
+  {id:18,name:"Acqua Man"},
+  {id:19,name:"Thanos"},
+  {id:20,name:"Flash"},
+  {id:21,name:"Spider Man"}
+
+]
